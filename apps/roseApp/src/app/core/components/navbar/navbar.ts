e08468b42f89278737993d223c926971e823e54f
OmarKhaled192 <okoros192000@gmail.com>
2026-06-19 05:02:14 +0300

    integrate

--- apps/roseApp/src/app/core/components/navbar/navbar.ts
+++ apps/roseApp/src/app/core/components/navbar/navbar.ts
@@ -1,18 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DarkModeComponent, LanguageSwitcherComponent, ToastMsg } from '@org/ui';
 
 @Component({
   selector: 'app-navbar',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, DarkModeComponent, LanguageSwitcherComponent, ToastMsg],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   searchQuery = '';
   isArabic = true;
-  mobileSearchOpen = false;
 
+  mobileSearchOpen = false;
   toggleMobileSearch(): void {
     this.mobileSearchOpen = !this.mobileSearchOpen;
   }
