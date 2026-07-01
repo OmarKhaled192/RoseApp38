5cf4eb00bda292f3705eec41e1abffdcd4eadb43
Tasneem-Gad <tgad@sirius-its.com>
2026-07-01 03:07:31 +0300

    Feature add Product details and Product review and interceptor

---
+++ apps/roseApp/src/app/features/product/services/review.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '@org/data-access';
+import { HttpClient } from '@angular/common/http';
+import { Review } from '../models/review';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ReviewService extends ApiService<Review> {
+  protected override endpoint = 'reviews';
+
+  constructor( http: HttpClient) {
+    super(http);
+  }
+
+}
