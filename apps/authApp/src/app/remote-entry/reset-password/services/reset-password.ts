2dc3d968cb6cad6f96902ade79d7d691b6b85baf
Tasneem-Gad <tgad@sirius-its.com>
2026-08-16 02:35:37 +0200

    fix/import

--- apps/authApp/src/app/remote-entry/reset-password/services/reset-password.ts
+++ apps/authApp/src/app/remote-entry/reset-password/services/reset-password.ts
@@ -1,4 +1,4 @@
-import { inject,sInjectable } from '@angular/core';
+import { inject,Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '@org/data-access';
 import { ResetPassword } from '../models/reset-password';
