770bb7e409f06184a6e12e00eebcce83046676b9
Tasneem-Gad <tgad@sirius-its.com>
2026-09-02 13:00:55 +0200

    fix/ remove test

--- apps/adminDashboard/src/test-setup.ts
+++ apps/adminDashboard/src/test-setup.ts
@@ -2,19 +2,5 @@ import '@angular/compiler';
 import '@analogjs/vitest-angular/setup-snapshots';
 import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
 
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: (query: string) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: () => {},
-    removeListener: () => {},
-    addEventListener: () => {},
-    removeEventListener: () => {},
-    dispatchEvent: () => false,
-  }),
-});
-
 setupTestBed();
 
