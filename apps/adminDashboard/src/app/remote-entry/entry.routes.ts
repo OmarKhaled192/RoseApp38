a481718e64e2384e747ff01f1de134db2be8e411
OmarKhaled192 <okoros192000@gmail.com>
2026-09-04 01:31:05 +0300

    solve-route

--- apps/adminDashboard/src/app/remote-entry/entry.routes.ts
+++ apps/adminDashboard/src/app/remote-entry/entry.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 import { DashboardLayout } from '../features/dashboard-layout/dashboard-layout';
-
+0
 export const remoteRoutes: Route[] = [
     {
         path: '',
