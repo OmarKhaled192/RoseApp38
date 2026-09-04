950963f06b40920ceadc0fb1a8ec61be3c6fd4e9
Tasneem-Gad <tgad@sirius-its.com>
2026-09-04 13:58:51 +0200

    fix lint

--- apps/adminDashboard/src/app/remote-entry/entry.routes.ts
+++ apps/adminDashboard/src/app/remote-entry/entry.routes.ts
@@ -1,11 +1,9 @@
 import { Route } from '@angular/router';
 import { DashboardLayout } from '../features/dashboard-layout/dashboard-layout';
-0
+
 export const remoteRoutes: Route[] = [
     {
         path: '',
         component: DashboardLayout,
-
     }
-
 ];
