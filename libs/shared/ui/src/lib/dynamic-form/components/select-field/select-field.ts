c6f00f0e77df5e49bf33631bceee4f62b7e3cd22
Tasneem-Gad <tgad@sirius-its.com>
2026-09-15 23:48:25 +0200

    fix translate

--- libs/shared/ui/src/lib/dynamic-form/components/select-field/select-field.ts
+++ libs/shared/ui/src/lib/dynamic-form/components/select-field/select-field.ts
@@ -1,19 +1,27 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { FormField ,FieldTree } from '@angular/forms/signals';
 import { SelectModule } from 'primeng/select';
 import { FieldConfig } from 'libs/shared/ui/src/models/field-types';
+import {  TranslatePipe, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'lib-select-field',
-  imports: [FormField, SelectModule],
+  imports: [FormField, SelectModule , TranslatePipe],
   template: `
   <div class="flex flex-col gap-1 w-full">
   <label class="text-sm font-medium text-gray-700" [for]="field().key">{{ field().label }}{{ field().required ? ' *' : '' }}</label>
 
-  <p-select [inputId]="field().key" [formField]="$any(control())" [options]="field().options" optionLabel="label"
-    optionValue="value" placeholder="اختر..." />
+<p-select
+  [dir]="translate.currentLang() === 'ar' ? 'rtl' : 'ltr'"
+  [inputId]="field().key"
+  [formField]="$any(control())"
+  [options]="field().options"
+  optionLabel="label"
+  optionValue="value"
+  [placeholder]="'form.selectPlaceholder' | translate" />
 </div>`
 })
 export class SelectField {
+   translate = inject(TranslateService);
   field = input.required<Extract<FieldConfig, { type: 'select' }>>();
   control = input.required<FieldTree<string>>();
 }
